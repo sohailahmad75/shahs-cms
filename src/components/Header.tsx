f3afbbda1e83806cda3149d99072e67265455098
Sohail Ahmad <sohail.bsse01@gmail.com>
2025-06-26 09:26:26 +0100

    sidebar changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
+interface Props {
+  isMobile: boolean;
+  openSidebar: () => void;
+}
 
-
-export default function Header() {
-    return (
-        <div className="flex h-screen">
-        Header
-            </div>
-    );
+export default function Header({ isMobile, openSidebar }: Props) {
+  return (
+    <header className="flex items-center justify-between bg-red-600 text-white px-4 py-3 shadow-md">
+      {isMobile && (
+        <button onClick={openSidebar}>
+          <img
+            src="/assets/img/icons/menu.svg"
+            className="h-6 w-6"
+            alt="Menu"
+          />
+        </button>
+      )}
+      <div className="flex items-center justify-between gap-2">
+        <div className="text-lg font-semibold">location dropdown</div>
+        <div className="text-lg font-semibold">User</div>
+      </div>
+    </header>
+  );
 }
