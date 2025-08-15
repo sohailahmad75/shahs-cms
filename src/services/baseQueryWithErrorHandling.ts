aa07bc03e9db0d05f7a4388e3c695ac0016b7fcc
Muhammad Usman <htimest@gmail.com>
2025-08-15 05:41:08 +0500

    changes for local storafge

--- src/services/baseQueryWithErrorHandling.ts
+++ src/services/baseQueryWithErrorHandling.ts
@@ -32,8 +32,7 @@ export const baseQueryWithErrorHandling: BaseQueryFn<
 > = async (args, api, extraOptions) => {
   const result = await rawBaseQuery(args, api, extraOptions);
 
-  
-   if (result.error?.status === 401 && localStorage.getItem("token")) {
+    if (result.error?.status === 401) {
     localStorage.removeItem("token");
     toast.error("Session expired. Please login again.");
     window.location.href = "/login";
