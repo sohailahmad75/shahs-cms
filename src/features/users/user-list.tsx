ea225f02613ed97793003918aebf443ff4a070c8
Muhammad Usman <htimest@gmail.com>
2025-09-10 23:53:05 +0500

    changes

--- src/features/users/user-list.tsx
+++ src/features/users/user-list.tsx
@@ -1,13 +1,17 @@
-// src/pages/users/helper/user-list.ts
 export const userFiltersConfig = [
+  {
+    key: 'createdAt',
+    label: 'Created At',
+    type: 'date',
+  },
   {
     key: "role",
     label: "Role",
     type: "select",
     options: [
       { value: "", label: "All Roles" },
-      { value: "owner", label: "Owner" },
-      { value: "staff", label: "Staff" },
+      { value: "1", label: "Owner" },
+      { value: "3", label: "Staff" },
     ],
   },
   {
@@ -16,14 +20,8 @@ export const userFiltersConfig = [
     type: "select",
     options: [
       { value: "", label: "All Statuses" },
-      { value: "active", label: "Active" },
-      { value: "inactive", label: "Inactive" },
+      { value: "1", label: "Active" },
+      { value: "2", label: "Inactive" },
     ],
-  },
-  {
-    key: "city",
-    label: "City",
-    type: "text",
-    placeholder: "Filter by city",
   },
 ];
