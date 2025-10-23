93dfc5fe9becca65aa561afb53e961481e80f207
Muhammad Usman <htimest@gmail.com>
2025-10-23 12:40:24 +0500

    changes for Filters

--- src/features/sales/products/helper/product-list.ts
+++ src/features/sales/products/helper/product-list.ts
@@ -19,6 +19,25 @@ export const productFiltersConfig = [
   },
   { key: "minPrice", label: "Min Price", type: "number" },
   { key: "maxPrice", label: "Max Price", type: "number" },
+  {
+    key: "productType",
+    label: "Product Type",
+    type: "select",
+    options: [
+      { label: "Stock", value: "stock" },
+      { label: "Non-stock", value: "non-stock" },
+      { label: "Service", value: "service" },
+    ],
+  },
+  {
+    key: "usage",
+    label: "Usage",
+    type: "select",
+    options: [
+      { label: "Store", value: "store" },
+      { label: "Warehouse", value: "warehouse" },
+    ],
+  },
 
   {
     key: "categoryId",
